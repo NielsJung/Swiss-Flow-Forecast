1b54b7371ce0e1c4c8da3151a49f998f7f2fe995
NielsJung <jung_niels@web.de>
2024-02-01 16:59:02 +0100

    RollOutTime

--- pages/Impressum.py
+++ pages/Impressum.py
@@ -6,7 +6,7 @@ st.set_page_config(
 #cwd = os.getcwd()
 # Streamlit App
 st.header('Impressum', divider='rainbow')
-st.text("Niels Jung")
-st.text("jung_niels@web.de")
+st.write("Niels Jung")
+st.write("jung_niels@web.de")
 
 
---
+++ pages/Model.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Model",
+)
+#cwd = os.getcwd()
+# Streamlit App
+st.header('Modeling', divider='rainbow')
+st.write("The live hydro data of the rivers and the weather forecasts of various weather models (ICON, GFS, JMA, GEM) are queried every hour  and saved in a database. The shown model then predicts the hourly discharge for the next 5 days based on the different weather forecasts and the measured discharges. For the prediction, the model uses the following features from the weatherforecasts:")
+st.write("• Temperature")
+st.write("• Snowdepth")
+st.write("• Precipitation")
+st.write("• Snowfall")
+st.write("• Soil Moisture")
+st.write("• Cloud Cover")
+st.write("A separate forecast is created and displayed for each weather model.")
+img_path="data\Model Architecture.jpg"
+st.image(img_path, caption="Model Architecture", use_column_width=True)
