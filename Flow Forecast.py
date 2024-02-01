527101a6c9687aadf519194202981cda3d3d2687
NielsJung <jung_niels@web.de>
2024-02-01 17:53:11 +0100

    NewQueryFunction

--- Flow Forecast.py
+++ Flow Forecast.py
@@ -15,33 +15,9 @@ def dict_to_df(data_dict):
     df.index = pd.to_datetime(df.index)
     return df
 
-def query_forecast_times():
-    
-    with open(r'data/PlotCreationTime.json', 'r', encoding='utf-8') as json_file:
-        data_dict = json.load(json_file)
-    station_ids, forecast_times={},{}
-    for river in data_dict:
-        station_ids[river]=data_dict[river]["StationID"]
-        forecast_times[river]=data_dict[river]["Forecasttime"]
 
-    return station_ids, forecast_times
 
-def search_for_forecast_plot(river,station_id, forecast_time):
-    
-    img_folder="data/Plots"
-    aktueller_zeitstempel = datetime.now()
-    forecast_time=datetime.strptime(forecast_time, '%Y-%m-%d %H:%M:%S')
-
-    timedelta=(aktueller_zeitstempel - forecast_time).total_seconds()
-
-    img_path=img_folder+"/"+river+".png"
-    if timedelta>=3600:
-        create_new_plot(station_id, img_path)
-        forecast_time=update_forecast_time(river)
-
-    return img_path, str(forecast_time)
-
-def create_new_plot(station_id, img_path):
+def create_new_plot(station_id):
     river_data=query_forecast(station_id)
 
     measurements=river_data["Measurements"]
@@ -79,18 +55,10 @@ def create_new_plot(station_id, img_path):
     ax.set_xlim(start, stopp)
     ax.set_ylim(0, np.max([10, 1.1*max_value]))
     ax.set_ylabel( "Flow [$m^3/s$]")
-    plt.savefig(img_path, dpi=800)
-    plt.close()
+    aktueller_zeitstempel = datetime.now()
+    ax.set_title("Forecasttime: "+str(aktueller_zeitstempel)[:19])
+    return fig
 
-def update_forecast_time(river):
-
-    with open(r'data/PlotCreationTime.json', 'r', encoding='utf-8') as json_file:
-        data_dict = json.load(json_file)
-    data_dict[river]["Forecasttime"]=str(datetime.now())[:-7]
-    with open(r'data/PlotCreationTime.json', 'w', encoding='utf-8') as json_file:
-        json.dump(data_dict, json_file, indent=2, ensure_ascii=False)
-
-    return data_dict[river]["Forecasttime"]
 
 
 def query_avilable_rivers():
@@ -151,18 +119,17 @@ st.set_page_config(
 # Streamlit App
 st.title('Swiss Riverflow Forecast')
 
-station_ids, forecast_times=query_forecast_times()
-rivers=list(forecast_times.keys())
+station_ids=query_avilable_rivers()
 
-
+rivers=list(station_ids.keys())
 
 
 # Auswahl des Plots
 choosen_river = st.selectbox('Rivers:',rivers )
-img_path, new_forecast_time=search_for_forecast_plot(choosen_river,station_ids[choosen_river], forecast_times[choosen_river])
-forecast_times[choosen_river]=new_forecast_time
+fig=create_new_plot(station_ids[choosen_river])
+st.pyplot(fig)
 
-st.image(img_path, caption="Forecasttime: "+forecast_times[choosen_river], use_column_width=True)
+
 
 
 
